64f5570a1a1fd1c2e613aa31c8e432f6f72c5c5a
khad.work20@gmail.com <khd.work20@gmail.com>
2020-09-16 20:15:42 +0700

    portfolio

---
+++ js/script.js
@@ -0,0 +1,20 @@
+const sideNav = document.querySelectorAll(".sidenav");
+M.Sidenav.init(sideNav);
+
+const slide = document.querySelectorAll(".slider");
+M.Slider.init(slide,{
+    indicators: false,
+    height: 420,
+    duration: 1000,
+});
+
+const parallax = document.querySelectorAll(".parallax");
+M.Parallax.init(parallax);
+
+const material = document.querySelectorAll(".materialboxed");
+M.Materialbox.init(material);
+
+const scroll = document.querySelectorAll(".scrollspy");
+M.ScrollSpy.init(scroll, {
+  scrollOffset: 40
+});
